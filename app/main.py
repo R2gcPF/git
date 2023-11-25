64427442ccffa29618e2907355837b8eba7ba218
idesh <ideshiori@outlook.jp>
2023-11-25 23:19:42 +0900

    fix

--- app/main.py
+++ app/main.py
@@ -11,17 +11,12 @@ def get_rand_df():
 
 def main():
     df = get_rand_df()
-    # c_left, c_right = st.columns(2)
     st.title(f'ENTITY {df["lat"][0]} {df["lon"][0]}')
-    # with c_left:
     btn = st.button('RELOAD')
-    # with c_right:
-        # btn2 = st.button('RELOAD2')
     map_main = st.map(df)
     map_sub = st.map(df, zoom=0)
-    # c_right.map_sub = st.map(df, zoom=0)
     if btn:
-        df = get_rand_df()
+        st.rerun()
 
 if __name__ == '__main__':
     main()
